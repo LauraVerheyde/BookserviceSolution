7ab700d99a96e7aa58e10554153bb1e36aae8873
L Verheyde <l.verheyde@outlook.be>
2018-10-18 10:58:06 +0200

    Update Publisher model with constructors

--- Bookservice.WebAPI/Models/Publisher.cs
+++ Bookservice.WebAPI/Models/Publisher.cs
@@ -11,5 +11,22 @@ namespace Bookservice.WebAPI.Models
         public string Name { get; set; }
         public string Country { get; set; }
 
+        public Publisher()
+        {
+            //leeg omdat als het niet leeg is, dan zou entity telkens verwachten dat er data moet inzitten
+            //zeker parameterloze ctor in het leven roepen
+        }
+        
+
+        //met deze constructor is het eenvoudiger om data in te voeren, niet telkens typen:
+        // Id = 2, Name = "xxx", Country ="xxx"
+        public Publisher(int id, string name, string country)
+        {
+            Id = id;
+            Name = name;
+            Country = country;
+        }
+        
+
     }
 }
